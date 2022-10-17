1b23f845a6382be45d4b9127e3f22b49652c8df2
JiashengLi <1013576838@qq.com>
2022-10-17 13:04:38 +0800

    updated data

---
+++ Recipe/README.md
@@ -0,0 +1,1 @@
+# deco3500_balenciaga
--- Recipe/Recipe/js/mockdata.js
+++ Recipe/Recipe/js/mockdata.js
@@ -1,11 +1,3 @@
-/*
- * @Author: wyf 844650674@qq.com
- * @Date: 2022-10-12 19:49:09
- * @LastEditors: wyf 844650674@qq.com
- * @LastEditTime: 2022-10-13 14:14:40
- * @FilePath: \app\js\mockdata.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 let foods = [
   {
     name: "Seafood",
